3dd48268d40a182e98bee835d04bc104e861d37a
PedroGivt <pedro@givtapp.net>
2023-03-06 16:14:12 +0000

    add notes

--- Givt.Backend.Bff.Retool.Service/Controllers/Notification/v1/NotificationController.cs
+++ Givt.Backend.Bff.Retool.Service/Controllers/Notification/v1/NotificationController.cs
@@ -4,13 +4,13 @@ namespace Givt.Backend.Bff.Retool.Service.Controllers.Notification.v1
 {
     [ApiController]
     [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    public class BackOfficeController : ControllerBase
+    [Route("api/v{version:apiVersion}/[controller]")] //either use this a version 1.0, 2.0, 3.0, ... or the folder style
+    public class NotificationController : ControllerBase
     {
         private readonly INotificationService _notificationService;
 
 
-        public BackOfficeController(INotificationService notificationService)
+        public NotificationController(INotificationService notificationService)
         {
             _notificationService = notificationService;
         }
